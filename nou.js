cfc41881fcb0e25dc92aab056a46fbaf5fced88b
Juli <2024_juli.farres@iticbcn.cat>
2025-10-07 19:39:01 +0200

    nou fitxer per company pujat

---
+++ nou.js
@@ -0,0 +1,13 @@
+function convertirCF() {
+    let CF = prompt("A què vols convertir C/F?");
+    let temp = parseFloat(prompt("Temperatura:"));
+    if( CF.toUpperCase() == "C" ){
+        let cent = (temp-32)/(5/9);
+        console.log(`${temp}F -> ${cent}C`);
+    }else if (CF.toUpperCase() == "F"){
+        let fh = (5/9) * temp + 32;
+        console.log(`${temp}C -> ${fh}F`);
+    }else {
+        console.log("Només es pot C ó F");
+    }
+}
